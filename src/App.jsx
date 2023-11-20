e1ce80bc65d6183ba4d4c44dd9d88ed49dde9b06
krn-7717 <104199555+krn-7717@users.noreply.github.com>
2023-11-20 14:21:08 +0900

    [finish]react03

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import './App.css'
  */
 export const App = () => {
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <><meta name="description" content="犬の画像を表示するサイトです" /><header>hello react!</header><img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" alt="犬の画像" /></>
   )
 }
